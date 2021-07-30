7b504a6e23690b39811e19c98275f6463ab90c1c
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-07-30 22:58:13 +0500

    Update models.py

--- turboHrBot/turboHrBotApp/models.py
+++ turboHrBot/turboHrBotApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Attendance(models.Model):
+    UserId = models.IntegerField(
+        verbose_name='Telegram User Identification Number'
+    )
+    UserName = models.TextField(
+        verbose_name='Telegram User Name'
+    )
+    UserFullName = models.TextField(
+        verbose_name='Telegram User Full Name'
+    )
+    TimeStamp = models.DateTimeField(
+        verbose_name='Date'
+    )
+    StartDate = models.DateTimeField(
+        verbose_name='Start Time of Work'
+    )
+    EndDate = models.DateTimeField(
+        verbose_name='End Time of Work'
+    )
+     
