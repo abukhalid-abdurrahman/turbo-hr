190f43ed754f7cabee11c7f98c540cade732c455
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-04 20:53:31 +0500

    user event log finished

--- turboHrBot/turboHrBotApp/services/UserEventLogService.py
+++ turboHrBot/turboHrBotApp/services/UserEventLogService.py
@@ -1,10 +1,90 @@
+import csv
+from datetime import datetime
+from django.core.paginator import Paginator
+from django.http.response import HttpResponse
+import xlwt
+from turboHrBotApp.models import UserEventLog
+
+
 class UserEventLogService():
 
-    def GetUserEventLogs(self, request):
-        pass
+    def GetUserEventLogs(self, page, name, date):
+        sqlRawQuery = 'SELECT * FROM turboHrBotApp_usereventLog'
 
-    def ExportUserEventLogsExcel(self, request):
-        pass
+        if name is not None and name is not '':
+            sqlRawQuery += f" WHERE UserFullName = '{name}'"
+        if date is not None and date is not '':
+            if 'WHERE' in sqlRawQuery:
+                sqlRawQuery += f" AND TimeStamp = date('{date}')"
+            else:
+                sqlRawQuery += f" WHERE TimeStamp = date('{date}')"
 
-    def ExportUserEventLogsCsv(self, request):
-        pass
+        logs = UserEventLog.objects.raw(sqlRawQuery)
+        paginatorInstance = Paginator(logs, 5)
+        pageNumber = page
+        logs = paginatorInstance.get_page(pageNumber)
+        return logs
+
+    def ExportUserEventLogsExcel(self, name, date):
+        response=HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = f'attachment; filename=User Event Log-{str(datetime.now())}.xls'
+
+        workBook = xlwt.Workbook(encoding='utf-8')
+        workSheet = workBook.add_sheet('User Event Log')
+        rowIterator = 0
+        fontStyle = xlwt.XFStyle()
+        fontStyle.font.bold = True
+
+        columns = [
+            'Log Id',
+            'Full Name', 
+            'Date', 
+            'Event' ]
+        
+        for columnIterator in range(len(columns)):
+            workSheet.write(rowIterator, columnIterator, columns[columnIterator], fontStyle)
+
+        fontStyle = xlwt.XFStyle()
+
+        sqlRawQuery = 'SELECT * FROM turboHrBotApp_usereventLog'
+        if name is not None and name is not '':
+            sqlRawQuery += f" WHERE UserFullName = '{name}'"
+        if date is not None and date is not '':
+            if 'WHERE' in sqlRawQuery:
+                sqlRawQuery += f" AND TimeStamp = date('{date}')"
+            else:
+                sqlRawQuery += f" WHERE TimeStamp = date('{date}')"
+
+        logs = UserEventLog.objects.raw(sqlRawQuery)
+
+        for item in logs:
+            rowIterator += 1
+            columnIterator = 0
+            workSheet.write(rowIterator, columnIterator, str(item.id), fontStyle)
+            workSheet.write(rowIterator, columnIterator + 1, str(item.UserFullName), fontStyle)
+            workSheet.write(rowIterator, columnIterator + 2, str(item.TimeStamp), fontStyle)
+            workSheet.write(rowIterator, columnIterator + 3, str(item.Event), fontStyle)
+        workBook.save(response)
+        return response
+
+    def ExportUserEventLogsCsv(self, name, date):
+        response=HttpResponse(content_type='application/text/csv')
+        response['Content-Disposition'] = f'attachment; filename=User Event Log-{str(datetime.now())}.csv'
+
+        writer = csv.writer(response)
+        writer.writerow(['Log Id', 'User Full Name', 'Date', 'Event'])
+
+        sqlRawQuery = 'SELECT * FROM turboHrBotApp_usereventLog'
+        if name is not None and name is not '':
+            sqlRawQuery += f" WHERE UserFullName = '{name}'"
+        if date is not None and date is not '':
+            if 'WHERE' in sqlRawQuery:
+                sqlRawQuery += f" AND TimeStamp = date('{date}')"
+            else:
+                sqlRawQuery += f" WHERE TimeStamp = date('{date}')"
+
+        logs = UserEventLog.objects.raw(sqlRawQuery)
+
+        for log in logs:
+            writer.writerow([str(log.id), str(log.UserFullName), str(log.TimeStamp), str(log.Event)])
+        return response
--- turboHrBot/turboHrBotApp/views.py
+++ turboHrBot/turboHrBotApp/views.py
@@ -1,28 +1,20 @@
 from datetime import datetime
+from django.http import response
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Attendance, UserEventLog
+from services import UserEventLogService
 import xlwt
 import csv
 
+userEventLogService = UserEventLogService()
+
 def logs(request):
-    sqlRawQuery = 'SELECT * FROM turboHrBotApp_usereventLog'
     fullNameFilter = request.GET.get('name-filter')
-    if fullNameFilter is not None and fullNameFilter is not '':
-        sqlRawQuery += f" WHERE UserFullName = '{fullNameFilter}'"
     dateFilter = request.GET.get('date-filter')
-    if dateFilter is not None and dateFilter is not '':
-        if 'WHERE' in sqlRawQuery:
-            sqlRawQuery += f" AND TimeStamp = date('{dateFilter}')"
-        else:
-            sqlRawQuery += f" WHERE TimeStamp = date('{dateFilter}')"
-
-    logs = UserEventLog.objects.raw(sqlRawQuery)
-    paginatorInstance = Paginator(logs, 5)
-    pageNumber = request.GET.get('page', 1)
-    logs = paginatorInstance.get_page(pageNumber)
-
+    page = request.GET.get('page', 1)
+    logs = userEventLogService.GetUserEventLogs(page, fullNameFilter, dateFilter)
     return render(request, 'logs.html', { 
         'data': logs,
         'fullNameFilter': '' if fullNameFilter is None else fullNameFilter,
@@ -61,49 +53,10 @@ def attendance(request):
 
 
 def exportLogsExcel(request):
-    response=HttpResponse(content_type='application/ms-excel')
-    response['Content-Disposition'] = f'attachment; filename=User Event Log-{str(datetime.now())}.xls'
-
-    workBook = xlwt.Workbook(encoding='utf-8')
-    workSheet = workBook.add_sheet('User Event Log')
-    rowIterator = 0
-    fontStyle = xlwt.XFStyle()
-    fontStyle.font.bold = True
-
-    columns = [
-        'Log Id',
-        'Full Name', 
-        'Date', 
-        'Event' ]
-    
-    for columnIterator in range(len(columns)):
-        workSheet.write(rowIterator, columnIterator, columns[columnIterator], fontStyle)
-
-    fontStyle = xlwt.XFStyle()
-
-    sqlRawQuery = 'SELECT * FROM turboHrBotApp_usereventLog'
-    fullNameFilter = request.GET.get('name-filter')
-    if fullNameFilter is not None and fullNameFilter is not '':
-        sqlRawQuery += f" WHERE UserFullName = '{fullNameFilter}'"
     dateFilter = request.GET.get('date-filter')
-    if dateFilter is not None and dateFilter is not '':
-        if 'WHERE' in sqlRawQuery:
-            sqlRawQuery += f" AND TimeStamp = date('{dateFilter}')"
-        else:
-            sqlRawQuery += f" WHERE TimeStamp = date('{dateFilter}')"
-
-    logs = UserEventLog.objects.raw(sqlRawQuery)
-
-    for item in logs:
-        rowIterator += 1
-        columnIterator = 0
-        workSheet.write(rowIterator, columnIterator, str(item.id), fontStyle)
-        workSheet.write(rowIterator, columnIterator + 1, str(item.UserFullName), fontStyle)
-        workSheet.write(rowIterator, columnIterator + 2, str(item.TimeStamp), fontStyle)
-        workSheet.write(rowIterator, columnIterator + 3, str(item.Event), fontStyle)
-    workBook.save(response)
+    fullNameFilter = request.GET.get('name-filter')
+    response = UserEventLogService.ExportUserEventLogsExcel(fullNameFilter, dateFilter)
     return response
-
 
 def exportExcel(request):
     response=HttpResponse(content_type='application/ms-excel')
@@ -167,27 +120,9 @@ def exportExcel(request):
     return response
 
 def exportLogsCsv(request):
-    response=HttpResponse(content_type='application/text/csv')
-    response['Content-Disposition'] = f'attachment; filename=User Event Log-{str(datetime.now())}.csv'
-
-    writer = csv.writer(response)
-    writer.writerow(['Log Id', 'User Full Name', 'Date', 'Event'])
-
-    sqlRawQuery = 'SELECT * FROM turboHrBotApp_usereventLog'
     fullNameFilter = request.GET.get('name-filter')
-    if fullNameFilter is not None and fullNameFilter is not '':
-        sqlRawQuery += f" WHERE UserFullName = '{fullNameFilter}'"
     dateFilter = request.GET.get('date-filter')
-    if dateFilter is not None and dateFilter is not '':
-        if 'WHERE' in sqlRawQuery:
-            sqlRawQuery += f" AND TimeStamp = date('{dateFilter}')"
-        else:
-            sqlRawQuery += f" WHERE TimeStamp = date('{dateFilter}')"
-
-    logs = UserEventLog.objects.raw(sqlRawQuery)
-
-    for log in logs:
-        writer.writerow([str(log.id), str(log.UserFullName), str(log.TimeStamp), str(log.Event)])
+    response = userEventLogService.ExportUserEventLogsCsv(fullNameFilter, dateFilter)
     return response
 
 def exportAttendanceCsv(request):
