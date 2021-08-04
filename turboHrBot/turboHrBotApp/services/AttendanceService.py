c63d4ebfb08f5a4bf5b885599271124d64ee0835
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-04 20:41:35 +0500

    added services

---
+++ turboHrBot/turboHrBotApp/services/AttendanceService.py
@@ -0,0 +1,10 @@
+class AttendanceService():
+
+    def GetAttendances(self, request):
+        pass
+
+    def ExportAttendanceExcel(self, request):
+        pass
+
+    def ExportAttendanceCsv(self, request):
+        pass
---
+++ turboHrBot/turboHrBotApp/services/UserEventLogService.py
@@ -0,0 +1,10 @@
+class UserEventLogService():
+
+    def GetUserEventLogs(self, request):
+        pass
+
+    def ExportUserEventLogsExcel(self, request):
+        pass
+
+    def ExportUserEventLogsCsv(self, request):
+        pass
